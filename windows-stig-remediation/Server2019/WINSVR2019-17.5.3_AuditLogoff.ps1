34e7cc1c1eadbc936427f4830209d670d00d4264
Eric Russo <russoee@gmail.com>
2025-05-04 20:43:41 -0400

    Update WINSVR2019-17.5.3_AuditLogoff.ps1

--- windows-stig-remediation/Server2019/WINSVR2019-17.5.3_AuditLogoff.ps1
+++ windows-stig-remediation/Server2019/WINSVR2019-17.5.3_AuditLogoff.ps1
@@ -1,7 +1,7 @@
 <#
 .SYNOPSIS
     This PowerShell script enables auditing for logoff events (success and failure),
-    in compliance with DISA STIG ID 17.5.2.
+    in compliance with DISA STIG ID 17.5.3.
 
 .NOTES
     Author          : Eric Russo
@@ -12,7 +12,7 @@
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
-    STIG-ID         : 17.5.2
+    STIG-ID         : 17.5.3
 
 .TESTED ON
     Date(s) Tested  : 2025-05-01
@@ -24,7 +24,7 @@
     Run this script as Administrator to enable auditing for user logoff events.
 
     Example:
-    PS C:\> .\17.5.2_AuditLogoff_SuccessFailure.ps1
+    PS C:\> .\17.5.3_AuditLogoff_SuccessFailure.ps1
 
     To verify:
     PS C:\> auditpol /get /subcategory:"Logoff"
@@ -51,4 +51,4 @@ if ($CurrentSetting -notmatch "Success" -or $CurrentSetting -notmatch "Failure")
 Write-Host "`nCurrent audit policy for Logoff:"
 auditpol /get /subcategory:"Logoff"
 
-Write-Host "`nSTIG 17.5.2 remediation complete."
+Write-Host "`nSTIG 17.5.3 remediation complete."
