a5887f689ba06e699236990de42ec3b602d01aa4
Eric Russo <russoee@gmail.com>
2025-05-07 21:20:42 -0400

    Update UBTU-24-400260_pwquality-complexity.sh

--- linux-remediation-scripts/UBTU-24-400260_pwquality-complexity.sh
+++ linux-remediation-scripts/UBTU-24-400260_pwquality-complexity.sh
@@ -2,22 +2,21 @@
 
 ###############################################################################
 # .SYNOPSIS
-#   Configures password complexity requirements using /etc/security/pwquality.conf
+#   Configures password complexity requirements using pwquality.conf
 #
 # .DESCRIPTION
-#   Enforces multiple DISA STIG requirements related to password complexity:
+#   Remediates the following STIGs:
 #   - UBTU-24-400260: Require uppercase characters
 #   - UBTU-24-400270: Require lowercase characters
 #   - UBTU-24-400280: Require digits
-#   - UBTU-24-400290: Require changed characters
-#   - UBTU-24-400320: Enforce minimum password length
+#   - UBTU-24-400290: Require difok = 8 (min changed characters)
+#   - UBTU-24-400320: Require minlen = 15
 #   - UBTU-24-400330: Require special characters
 #
 # .NOTES
 #   Author       : Eric Russo
-#   STIG IDs     : UBTU-24-400260, 400270, 400280, 400290, 400320, 400330
 #   Date Created : 2025-05-06
-#   Version      : 1.0
+#   Version      : 1.1 (portable syntax)
 #
 # .USAGE
 #   sudo ./UBTU-24-400260_pwquality-complexity.sh
@@ -27,32 +26,33 @@
 #
 ###############################################################################
 
+CONF_FILE="/etc/security/pwquality.conf"
+
+# Ensure we're running as root
 if [ "$EUID" -ne 0 ]; then
-  echo "[-] This script must be run as root. Exiting."
+  echo "[-] This script must be run as root."
   exit 1
 fi
 
-CONF_FILE="/etc/security/pwquality.conf"
+echo "[+] Updating $CONF_FILE with password complexity settings..."
 
-echo "[+] Configuring $CONF_FILE with required complexity parameters..."
-
-declare -A settings=(
-  ["ucredit"]="-1"
-  ["lcredit"]="-1"
-  ["dcredit"]="-1"
-  ["ocredit"]="-1"
-  ["difok"]="8"
-  ["minlen"]="15"
-)
-
-for key in "${!settings[@]}"; do
-  if grep -q "^$key" "$CONF_FILE"; then
-    sed -i "s/^$key.*/$key = ${settings[$key]}/" "$CONF_FILE"
-    echo "[✔] Updated: $key = ${settings[$key]}"
+update_or_append_setting() {
+  KEY="$1"
+  VALUE="$2"
+  if grep -q "^$KEY" "$CONF_FILE"; then
+    sed -i "s/^$KEY.*/$KEY = $VALUE/" "$CONF_FILE"
+    echo "[✔] Updated: $KEY = $VALUE"
   else
-    echo "$key = ${settings[$key]}" >> "$CONF_FILE"
-    echo "[✔] Added: $key = ${settings[$key]}"
+    echo "$KEY = $VALUE" >> "$CONF_FILE"
+    echo "[✔] Added: $KEY = $VALUE"
   fi
-done
+}
 
-echo "[✔] Password complexity policy configured successfully."
+update_or_append_setting "ucredit" "-1"
+update_or_append_setting "lcredit" "-1"
+update_or_append_setting "dcredit" "-1"
+update_or_append_setting "ocredit" "-1"
+update_or_append_setting "difok" "8"
+update_or_append_setting "minlen" "15"
+
+echo "[✔] All password complexity parameters set successfully."
